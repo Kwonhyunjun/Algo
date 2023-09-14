9807dba1934c89d8e7c3ec54ad4a069313f826f9
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2023-09-14 22:37:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 568 ms, Memory: 144484 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static ArrayList<Integer>[] adjList;
+	static boolean[] v;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		adjList = new ArrayList[N + 1];
+		v = new boolean[N + 1];
+
+		for (int i = 1; i < N + 1; i++) {
+			adjList[i] = new ArrayList();
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+
+			adjList[from].add(to);
+			adjList[to].add(from);
+
+		}
+
+		int cnt = 0; 
+		for (int i = 1; i < N + 1; i++) {
+			if (v[i])
+				continue;
+			cnt++; 
+			bfs(i);
+
+		}
+		System.out.println(cnt);
+
+	}
+
+	private static void bfs(int start) {
+		Queue<Integer> q = new ArrayDeque();
+		
+		q.offer(start); 
+		v[start] = true;
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			
+			for(int i=0, size = adjList[cur].size(); i<size; i++) {
+				if(v[adjList[cur].get(i)]) continue;
+				q.offer(adjList[cur].get(i)); 
+				v[adjList[cur].get(i)] = true;
+			}
+		}
+		
+	}
+
+}
