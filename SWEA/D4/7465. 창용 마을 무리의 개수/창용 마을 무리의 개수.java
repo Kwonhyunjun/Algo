a74938ba48334ed2d97d3ef9a375e1a598de159d
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2023-10-10 22:44:58 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 139 ms, Memory: 27,248 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,66 +1,68 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Solution {
-	static int N; // 원소의 개수
-	static int M; // 연산의 개수
-	static int[] parents;
-
+class Solution {
+	static int N, M, ans;
+	static List<Integer>[] adj; 
+	static boolean[] v;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder(); 
-
-		// 테스트 케이스 수 읽기
+		StringTokenizer st; 
+		
 		int T = Integer.parseInt(br.readLine());
-
-		// 테스트 케이스 수만큼 반복
-		for (int t = 1; t <= T; t++) {
+		for(int tc=1; tc<=T; tc++) {
 			st = new StringTokenizer(br.readLine());
-
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-
-			parents = new int[N + 1];
-
-			for (int i = 1; i < N + 1; i++) {
-				parents[i] = i; 
+			v = new boolean[N+1]; 
+			adj = new List[N+1];
+			ans = 0; 
+			for(int i=0; i<N+1; i++) {
+				adj[i] = new ArrayList<Integer>();
 			}
 			
-			int ans = 0; 
-			for (int i = 0; i < M; i++) {
+			for(int i=0; i<M; i++) {
 				st = new StringTokenizer(br.readLine());
-
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
 				
-				union(a, b); 
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				
+				adj[from].add(to);
+				adj[to].add(from);
 			}
 			
-			for(int i =1; i<parents.length;i++) {
-				if(i == parents[i]) ans++;
+			for(int i=1; i<N+1; i++) {
+				if(v[i]) continue; 
+				bfs(i);
+				ans++; 
 			}
-			System.out.printf("#%d %s\n",t ,ans);
+			System.out.printf("#%d %d\n", tc, ans);
 		}
 	}
-	
-	private static boolean union(int b, int c) {
-		b = find(b); 
-		c = find(c); 
-		
-		if(b == c) return false;
-		
-		parents[c] = b;
-		return true;
-	}
 
-	static int find(int a) {
-		if(a == parents[a]) return a; 
+	private static void bfs(int s) {
+		Queue<Integer> q = new ArrayDeque<>();
 		
-		return parents[a] = find(parents[a]); 
+		q.offer(s); 
+		v[s] = true; 
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			
+			int size = adj[cur].size(); 
+			for(int i=0; i<size; i++) {
+				int next = adj[cur].get(i);
+				if(v[next]) continue;
+				q.offer(next);
+				v[next] = true;
+			}
+		}
+		
 	}
 }
