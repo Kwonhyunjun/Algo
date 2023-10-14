41bada73f5d38b3237e738ed415388e2874b5e5f
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2023-10-14 11:12:26 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11304 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, ans;
+
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( \')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+	static void solve() {
+
+	}
+}
