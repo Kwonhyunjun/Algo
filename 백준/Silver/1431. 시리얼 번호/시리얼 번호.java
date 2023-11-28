e7f408055b96f350e0635a72079e6f2ee836d0d2
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2023-11-28 17:38:51 +0900

    [Silver III] Title: 시리얼 번호, Time: 84 ms, Memory: 11688 KB -BaekjoonHub

---
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+        int N = Integer.parseInt(br.readLine());
+
+        List<String> list = new ArrayList<>();
+
+        for(int i=0; i<N; i++){
+            list.add(br.readLine());
+        }
+
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() != o2.length()){
+                    return o1.length() - o2.length();
+                }else{
+                    int n1 = sum(o1);
+                    int n2 = sum(o2);
+                    if(n1 == n2) return o1.compareTo(o2);
+                    else return n1- n2;
+                }
+            }
+
+            private int sum(String str) {
+                int sum = 0;
+                for(int i=0; i<str.length(); i++){
+                    int num = str.charAt(i) - '0';
+                    if(num >= 0 && num <= 9 ) sum += num;
+                }
+                return sum;
+            }
+        });
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i) + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}
