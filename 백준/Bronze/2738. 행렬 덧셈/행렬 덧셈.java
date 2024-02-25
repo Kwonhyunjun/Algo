f9b1ffdadfe97453cc6fbc7fcc26e3c319275a95
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-02-25 09:35:56 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 292 ms, Memory: 15976 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,31 @@
+import java.io.*; 
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()); 
+		
+		int N = Integer.parseInt(st.nextToken()); 
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[][] map = new int[N][M];
+		
+		for(int i=0; i<N*2; i++) {
+			st = new StringTokenizer(br.readLine()); 
+			for(int j=0; j<M; j++) {
+				int r = i % N; 
+				map[r][j] += Integer.parseInt(st.nextToken()); 
+			}
+		}
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
