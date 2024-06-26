ae33fd22d81a683cfb04b227f70ab838dc4a962a
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-06-26 17:48:53 +0900

    [level 2] Title: 땅따먹기, Time: 19.07 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -8,41 +8,41 @@ class Solution {
         
         int[][] map = new int[R][C];
         
-        int first = 0; 
-        int firstIdx = 0; 
-        int second = 0; 
-        
-        for(int i=0; i<4; i++){
+        // 첫 번째 행 초기화
+        for (int i = 0; i < 4; i++) {
             map[0][i] = land[0][i];
         }
-        
-        for(int i=1; i<R; i++){
-            int cur = land[i][0]; 
-            map[i][0] = Math.max(cur+map[i-1][3], Math.max(cur+map[i-1][1], cur+map[i-1][2])); 
-            cur = land[i][1]; 
-            map[i][1] = Math.max(cur+map[i-1][3], Math.max(cur+map[i-1][0], cur+map[i-1][2])); 
-            cur = land[i][2];
-            map[i][2] = Math.max(cur+map[i-1][3], Math.max(cur+map[i-1][0], cur+map[i-1][1]));
-            cur = land[i][3];
-            map[i][3] = Math.max(cur+map[i-1][2], Math.max(cur+map[i-1][0], cur+map[i-1][1])); 
-        }
-        
-        // System.out.printf("%d %d %d", first, firstIdx, second); 
-        
-        // System.out.printf("%s\n", Arrays.toString(map[R-1]));
-        
-        for(int i=0; i<R; i++){
-                
-        }
-        
-        
-        answer = Integer.MIN_VALUE; 
-        
-        for(int i=0; i<4; i++){
-            answer = Math.max(answer, map[R-1][i]); 
-        }
-        
 
+        // 두 번째 행부터 처리
+        for (int i = 1; i < R; i++) {
+            int first = 0, second = 0, firstIdx = -1;
+            
+            // 이전 행의 최대값(first)과 두 번째로 큰 값(second) 찾기
+            for (int j = 0; j < 4; j++) {
+                if (map[i-1][j] > first) {
+                    second = first;
+                    first = map[i-1][j];
+                    firstIdx = j;
+                } else if (map[i-1][j] > second) {
+                    second = map[i-1][j];
+                }
+            }
+
+            // 현재 행 갱신
+            for (int j = 0; j < 4; j++) {
+                if (j == firstIdx) {
+                    map[i][j] = land[i][j] + second;
+                } else {
+                    map[i][j] = land[i][j] + first;
+                }
+            }
+        }
+        
+        // 마지막 행의 최대값 찾기
+        for (int i = 0; i < 4; i++) {
+            answer = Math.max(answer, map[R-1][i]);
+        }
+        
         return answer;
     }
 }
