09e33546dd7b90fa6c61e2b232c018fd6b466ffb
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-07-29 23:15:19 +0900

    [level 1] Title: 자동차 대여 기록에서 장기/단기 대여 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
+++ 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -1,15 +1,14 @@
 -- 코드를 입력하세요
-SELECT HISTORY_ID, CAR_ID, date_format(START_DATE, '%Y-%m-%d') START_DATE, date_format(END_DATE, '%Y-%m-%d') END_DATE,
-case 
-    when datediff(end_date, start_date)+1  >= 30 then '장기 대여'
-    else '단기 대여'
-end as RENT_TYPE
+SELECT HISTORY_ID, CAR_ID, DATE_FORMAT(START_DATE, '%Y-%m-%d') as START_DATE, DATE_FORMAT(END_DATE, '%Y-%m-%d') as END_DATE, 
+CASE 
+    WHEN DATEDIFF(END_DATE, START_DATE)+1 >= 30 THEN '장기 대여'
+    ELSE '단기 대여'
+END as 'RENT_TYPE'
 FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-WHERE year(START_DATE) = 2022 and month(START_DATE) = 9
-ORDER BY HISTORY_ID DESC ;
+WHERE YEAR(START_DATE) = 2022 AND MONTH(START_DATE) = 9
+ORDER BY HISTORY_ID DESC
 
-# select * from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-# WHERE year(START_DATE) = year(END_DATE) and month(START_DATE) = month(END_DATE) and day(START_DATE) = day(END_DATE); 
 
-# select datediff(end_date, start_date)from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-# WHERE history_id = 513;
+# SELECT HISTORY_ID, CAR_ID, DATE_FORMAT(START_DATE, '%Y-%m-%d') as START_DATE, DATE_FORMAT(END_DATE, '%Y-%m-%d') as END_DATE, 
+# DATEDIFF(END_DATE, START_DATE)
+# FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
