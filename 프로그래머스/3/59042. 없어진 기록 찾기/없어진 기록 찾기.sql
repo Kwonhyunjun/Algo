5b577071e6a98d7762a74c38fc5a467ee3b9e324
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-08-04 21:43:02 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,11 +1,5 @@
 -- 코드를 입력하세요
-# SELECT ANIMAL_ID, NAME 
-# FROM
-
-# select * from ANIMAL_INS where name = 'daisy'; 
-# select * from ANIMAL_OUTS where name = 'daisy'; 
-
-select B.ANIMAL_ID, B.NAME 
-from ANIMAL_INS A right join ANIMAL_OUTS B on A.ANIMAL_ID = B.ANIMAL_ID
-where A.ANIMAL_ID is null;
-
+SELECT b.ANIMAL_ID, b.NAME
+FROM ANIMAL_INS a RIGHT OUTER JOIN ANIMAL_OUTS b ON a.ANIMAL_ID = b.ANIMAL_ID
+WHERE a.ANIMAL_ID is null
+ORDER BY b.ANIMAL_ID
