dd021e8de46b2ff285cd5d7be1ec3046b28efd16
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-10-07 10:40:27 +0900

    [Silver III] Title: 주유소, Time: 324 ms, Memory: 42280 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -9,40 +9,55 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		int[] dist = new int[N]; 
-		int[] gas = new int[N]; 
+		long[] dist = new long[N]; 
+		long[] gas = new long[N]; 
 		
 		st = new StringTokenizer(br.readLine()); 
 		for(int i=1; i<N; i++) {
-			dist[i] = Integer.parseInt(st.nextToken()) + dist[i-1]; 
+			dist[i] = Long.parseLong(st.nextToken()); 
 		}
 		
 //		System.out.println(Arrays.toString(dist)); 
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
-			gas[i] = Integer.parseInt(st.nextToken()); 
+			gas[i] = Long.parseLong(st.nextToken()); 
 		}
 		
-		int[] dp = new int[N]; 
+		long[] dp = new long[N]; 
 		
-		Arrays.fill(dp, Integer.MAX_VALUE); 
+		Arrays.fill(dp, Long.MAX_VALUE); 
 		
 		dp[0] = 0; 
 		
-		for(int i=1; i<N; i++) {
-			
-			for(int j=i-1; j>=0; j--) {
-//				System.out.printf("%d %d => 거리 %d, 가스비 %d\n", i, j, (dist[i]-dist[j]), gas[j]);
-//				System.out.printf("이전 도시에서 든 가스 비용 %d + 이전 도시에서 현재 도시까지 가시비용 %d\n", dp[j], ((dist[i]-dist[j]) * gas[j])); 
-				dp[i] = Math.min(dp[i], dp[j] + ((dist[i]-dist[j]) * gas[j]) ); 
+//		for(int i=1; i<N; i++) {
+//			
+//			for(int j=i-1; j>=0; j--) {
+////				System.out.printf("%d %d => 거리 %d, 가스비 %d\n", i, j, (dist[i]-dist[j]), gas[j]);
+////				System.out.printf("이전 도시에서 든 가스 비용 %d + 이전 도시에서 현재 도시까지 가시비용 %d\n", dp[j], ((dist[i]-dist[j]) * gas[j])); 
+//				dp[i] = Math.min(dp[i], dp[j] +  ((long) (dist[i]-dist[j]) * gas[j]) ); 
+//				
+//			}
+//			
+//		}
+	
+//		System.out.println(Arrays.toString(dp));
+//		System.out.println(dp[N-1]);
+	
+		long curGas = gas[0]; 
+		long answer = 0; 
+//		System.out.println(Arrays.toString(dist)); 
+//		System.out.println(Arrays.toString(gas)); 
 				
-			}
+		for(int i=1; i<N; i++) {
+			curGas = (curGas > gas[i-1]) ? gas[i-1] : curGas; 
+//			System.out.printf("현재가스 %d 거리%d\n", curGas, dist[i]);
 			
+			answer += (dist[i] * curGas);
 		}
 		
-//		System.out.println(Arrays.toString(dp));
-		System.out.println(dp[N-1]);
+		System.out.println(answer);
+
 	}
 
 }
