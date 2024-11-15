b1bf42a154581391632f414632869352d17d9336
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-11-15 10:11:26 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -2,10 +2,15 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i=1; i<=n; i++){ 
-            if((n % i) == 0) answer += i; 
-        }
+        // System.out.println(Math.sqrt(n));
         
-        return answer; 
+        for(int i=1; i<=Math.sqrt(n); i++){
+            if(n % i == 0){
+                // System.out.println(i);
+                answer += i;
+                if(i != n/i) answer += n/i ;
+            }
+        }
+        return answer;
     }
 }
