3662e35f4ddeb3fbcc333d9699aed42c3223b57a
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-12-24 17:09:33 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 448 ms, Memory: 51944 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
@@ -1,11 +1,6 @@
 import java.io.*;
 import java.util.*; 
 
-/**
- * TreeMap은 기본적으로 키값으로 정렬되지만, 값에 대한 정렬을 지원하지는 않음
- * 
- */
-
 public class Main {
 
 	
@@ -14,11 +9,11 @@ public class Main {
 		StringTokenizer st; 
 		StringBuilder sb = new StringBuilder(); 
 		
-		TreeMap<Integer, Integer> map1 = new TreeMap<>(); 	// key, value = 문제번호 , 난이도
-		TreeMap<Integer, TreeMap<Integer, Integer>> map2 = new TreeMap<>(); 	// value, key = 난이도, 문제번호
+		TreeMap<Integer, Integer> map1 = new TreeMap<>();
+		TreeMap<Integer, TreeMap<Integer, Integer>> map2 = new TreeMap<>(); 
 		 
 		
-		int N = Integer.parseInt(br.readLine()); //문제의 개수
+		int N = Integer.parseInt(br.readLine()); 
 		int num = 0; int difficulty =0; 
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine()); 
@@ -33,8 +28,6 @@ public class Main {
 			
 			map2.get(difficulty).put(num, 0); 
 		}
-//		System.out.println("  map1: " + map1);
-//		System.out.println("  map2: " + map2);
 		
 		int M = Integer.parseInt(br.readLine()); 
 		
@@ -57,9 +50,7 @@ public class Main {
 			case "solved":
 				if(map1.size()==0)break; 
 				num = Integer.parseInt(st.nextToken());
-				int n = map1.get(num); // 난이도
-//				System.out.println("난이도 : " + n);
-//				System.out.println(map2.get(n).firstKey());
+				int n = map1.get(num);
 				map1.remove(num);
 				
 				if(map2.get(n).size() > 1) {
@@ -73,18 +64,15 @@ public class Main {
 				if(map1.size()==0) break; 
 				int type = Integer.parseInt(st.nextToken()); 
 				
-				if(type == 1) { // 가장 어려운 문제가 여러개라면 문제 번호가 큰 것으로 출력
+				if(type == 1) { 
 					
 					sb.append(map2.get(map2.lastKey()).lastKey()).append("\n");  
-				}else { // 가장 쉬운 문제가 여러 개라면 문제 번호가 작은 것을 출력
+				}else { 
 					
 					sb.append(map2.get(map2.firstKey()).firstKey()).append("\n");
 				}
 				break; 
 			}
-//			System.out.println(i);
-//			System.out.println("  map1: " + map1);
-//			System.out.println("  map1: " + map2);
 		}
 		
 		System.out.println(sb);
