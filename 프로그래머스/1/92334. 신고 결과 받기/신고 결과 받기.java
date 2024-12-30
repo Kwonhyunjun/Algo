742ec32960009848d047eb085ebf2b5bb38e0811
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-12-30 09:15:31 +0900

    [level 1] Title: 신고 결과 받기, Time: 147.25 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,46 +1,44 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
-    // report -> 이용자id, 신고한id
-    public int[] solution(String[] id_list, String[] report, int k) {
+    public int[] solution(String[] id_list, String[] reports, int k) {
         int[] answer = new int[id_list.length];
         
-        // 이용자id 와 본인을 신고한 플레이어를 저장하는 리스트
-        HashMap<String, HashSet<String>> map = new HashMap<>(); 
-        HashMap<String, Integer> idx = new HashMap<>(); 
+        HashMap<String, Integer> idx = new HashMap<>();
+        HashMap<String, HashSet<String>> map = new HashMap<>();
         
-        // 이용자와 신고 플레이어 리스트 초기화
-        for(int i=0; i<id_list.length; i++){
-            map.put(id_list[i], new HashSet<>()); 
-            idx.put(id_list[i], i); // 인덱스 초기화
+        int i = 0;
+        for(String id : id_list){
+            idx.put(id, i++);
         }
         
-        for(String str : report){
-            StringTokenizer st = new StringTokenizer(str); 
-            String er = st.nextToken(); // 신고한 사람
-            String ee = st.nextToken(); // 신고 당한 사람
+        StringTokenizer st;
+        for(String report : reports){
+            st = new StringTokenizer(report);
             
-            map.get(ee).add(er);
+            String from = st.nextToken();
+            String to = st.nextToken(); 
+    
+            if(!map.containsKey(to)){
+                map.put(to, new HashSet<>());
+            }
+           map.get(to).add(from);
         }
         
-        // for(String n : map.keySet()){
-        //     System.out.println(n + " " + map.get(n)); 
-        // }
+        for(Map.Entry<String, HashSet<String>> entry : map.entrySet()){
+            
+            String key = entry.getKey(); 
+            HashSet<String> value = entry.getValue(); 
         
-        // 신고 개수 세기
-        for(String name : map.keySet()){
-            // System.out.println(name +" "+map.get(name).size()); 
-            if(map.get(name).size() >= k){
-                // 결과 메일 날리기
-                for(String s : map.get(name)){
-                    int index = idx.get(s);
-                    // System.out.println(index); 
-                    answer[index]++; 
-                }
+            if(value.size() < k) continue; 
+            
+            for(String from : value){
+                answer[idx.get(from)]++;
             }
         }
         
-        // System.out.println(Arrays.toString(answer)); 
+        
+        
         return answer;
     }
 }
