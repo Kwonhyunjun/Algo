63e13f76fdb0a23816458fc3ffdc3a92f5fbfe59
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-01-03 04:14:57 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,20 +1,20 @@
 class Solution {
     public int solution(int[] arr) {
-        int answer = arr[0]; 
+        int answer = arr[0];
         
         for(int i=1; i<arr.length; i++){
             answer = lcm(answer, arr[i]);
-        }; 
+        }
         
         return answer;
     }
     
     int gcd(int a, int b){
-        if(b == 0) return a; 
+        if(b==0) return a;
         return gcd(b, a%b);
     }
     
     int lcm(int a, int b){
-        return a * (b / gcd(a, b)); 
+        return(a * (b / gcd(a, b)));
     }
 }
