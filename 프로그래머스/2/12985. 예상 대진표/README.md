37defccf26983c289122d642ac1500dc116439f3
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-01-02 21:27:10 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 0.01 ms
+메모리: 76.5 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 09일 01:04:32
+2025년 01월 02일 21:27:07
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,27 +2,13 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 0;
-        int first = 0; int second = 0; 
-        
-        if(a > b){
-            first = b-1; 
-            second = a-1; 
-        }else{
-            first = a-1; 
-            second = b-1;
-        }
-        
-        
-        while(true){
-            if(first == second -1 && (first /2 == second/2)){
-                answer++; 
-                break;
-            }
+        int answer = 1;
+
+        while(((a+1)/2) != ((b+1)/2) || Math.abs(a-b) != 1){
+            a = (a+1) / 2; 
+            b = (b+1) / 2; 
             
-            first = first / 2;
-            second = second / 2;
-            answer++; 
+            answer++;
         }
 
         return answer;
