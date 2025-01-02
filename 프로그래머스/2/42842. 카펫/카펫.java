20a2f70f85f095e60db799a6c26ac7e5fa656175
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-01-02 11:21:24 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,22 +1,18 @@
 import java.util.*;
 
 class Solution {
-   public static void main(String[] args) throws Exception {
-		System.out.println(Arrays.toString(solution(24,24)));
-	}
-	
-	public static int[] solution(int brown, int yellow) {
-        for(int i=1; i<5000; i++) {
-			for(int j=1; j<5000; j++) {
-				if(i < j) break; 
-				if(i * j == brown + yellow) {
-					int sizeB = i * 2 + j * 2 - 4; 
-					if(brown == sizeB) {
-						return new int[] {i, j};
-					}
-				}
-			}
-		}
-		return null;
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        
+        for(int i=1; i<=Math.sqrt(yellow); i++){
+            int row = yellow / i; 
+            if(yellow%i != 0) continue; 
+            
+            if((row+2+i) * 2 == brown){
+                answer = new int[]{row+2, i+2};
+                break;
+            }
+        }
+        return answer;
     }
 }
