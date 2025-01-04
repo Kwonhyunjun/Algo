aa4f068019a1610e50e099ca2234417ccebdfda8
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-01-04 15:24:30 +0900

    [level 2] Title: 괄호 회전하기, Time: 36.75 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,42 +1,44 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        int len = s.length();
         
-        for(int i=0; i<len; i++){
-            Stack<Character> stack = new Stack<>(); 
-            boolean isValid = true; 
+        StringBuilder sb = new StringBuilder(s);
+        for(int i=0; i<s.length(); i++){
+            if(validate(sb.toString())) answer++; 
+            String first = sb.substring(0, 1); 
+            String rest = sb.substring(1); 
             
-            for(int j=0; j<len; j++){
-                char ch = s.charAt((i+j) % len);
-                
-                if(ch == '[' || ch == '{' || ch == '('){
-                    stack.push(ch);
-                }else{
-                    if(stack.isEmpty()) {
-                        isValid = false; 
-                        break;
-                    }
-                    char c = stack.peek();
-                    if((ch == ']' && c == '[') ||
-                       (ch == '}' && c == '{') ||
-                       (ch == ')' && c == '(') 
-                      ){
-                        stack.pop();
-                    } else {
-                        isValid = false; 
-                        break; 
-                    }
-                }
-            }
-                
-            if (isValid && stack.isEmpty()){
-                answer++; 
-            }
+            sb.setLength(0); 
+            sb.append(rest).append(first);
         }
         
         return answer;
+    }
+    
+    static boolean validate(String str){
+        Stack<Character> stack = new Stack<>(); 
+        
+        for(char c : str.toCharArray()){
+            if(c == '[' || c == '(' || c == '{'){
+                stack.add(c); 
+                continue;
+            }
+            
+            if(stack.isEmpty()) return false;
+            
+            char top = stack.peek();
+            if((top == '[' && c == ']') || (top == '{' && c == '}') || (top == '(' && c == ')')){
+                stack.pop(); 
+                continue;
+            }else{
+                return false;
+            }
+        }
+        
+        if(!stack.isEmpty()) return false;
+        
+        return true;
     }
 }
