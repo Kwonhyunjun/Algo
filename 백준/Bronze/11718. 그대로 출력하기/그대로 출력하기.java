47ae4bf143816003bcbab52a2874e5933f6f2cbb
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-01-08 20:14:57 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 13820 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			System.out.println(sc.nextLine());
+		}
+
+	}
+
+}
