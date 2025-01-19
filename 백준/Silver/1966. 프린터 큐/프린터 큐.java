39055404b1c609d47e71b968c288254bb9b1f009
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-01-19 09:35:03 +0900

    [Silver III] Title: 프린터 큐, Time: 204 ms, Memory: 18288 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,55 +1,64 @@
 import java.io.*;
-import java.util.*; 
+import java.util.*;
 
 public class Main {
+	
+	static class Data{
+		int weight;
+		boolean isChecked; 
+		
+		public Data(int weight, boolean isChecked) {
+			this.weight = weight;
+			this.isChecked = isChecked; 
+		}
+	}
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st; 
-		
-		int answer = 0; 
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		
 		int T = Integer.parseInt(br.readLine());
 		
+		
 		for(int t=0; t<T; t++) {
-//			System.out.printf("t : %d\n", t); 
 			st = new StringTokenizer(br.readLine());
 			
-			int N = Integer.parseInt(st.nextToken()); // 문서의 개수
-			int M = Integer.parseInt(st.nextToken()); // 찾고 싶은 문서의 위치
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 			
-			Queue<int[]> q = new ArrayDeque<>();
-			int[] priorities = new int[N]; 
+			Queue<Data> q = new ArrayDeque<>();
+			PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> -(o1-o2));
 			
 			st = new StringTokenizer(br.readLine());
 			for(int i=0; i<N; i++) {
-				int priority = Integer.parseInt(st.nextToken());
-				q.add(new int[] {i, priority});
-				priorities[i] = priority; 
-			}
-			
-			Arrays.sort(priorities);
-//			System.out.println(Arrays.toString(priorities)); 
-			
-			int order = 1; 
-			int idx = N-1; 
-			while(!q.isEmpty()) {
-				int[] cur = q.poll();
-//				System.out.println("cur : " + Arrays.toString(cur)); 
 				
-				if(cur[1] == priorities[idx]) {
-//					System.out.println("삭제");
-					order++; 
-					idx--; 
-					if(cur[0] == M) {
-//						System.out.println("찾던 값");
-						System.out.println(order-1); 
-						break; 
+				boolean isChecked = (i==M) ? true : false; 
+				int weight = Integer.parseInt(st.nextToken());
+				q.offer(new Data(weight, isChecked));
+				pq.offer(weight);
+			 }
+			
+			int cnt = 1; 
+			
+			while(!q.isEmpty()) {
+				Data cur = q.poll();
+				
+				if(cur.weight == pq.peek()) {
+					if(cur.isChecked) {
+						sb.append(cnt).append("\n");
+						break;
+					}else {
+						cnt++;
+						pq.poll();
 					}
 				}else {
-//					System.out.println("다시 넣음");
-					q.add(cur); 
+					q.offer(cur);
 				}
 			}
 		}
+		
+		System.out.println(sb);
 	}
+
 }
