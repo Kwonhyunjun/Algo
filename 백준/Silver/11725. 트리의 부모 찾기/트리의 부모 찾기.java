9c1edc2fbf071d0bf182867e2295960570b5d66d
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-03-06 11:47:17 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 436 ms, Memory: 72484 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,61 +1,58 @@
 import java.io.*;
-import java.util.*;
+import java.util.*; 
 
 public class Main {
-	
-	static int N, parents[]; 
-	static boolean[] v; 
-	static ArrayList<Integer>[] adjList; 
 
-	public static void main(String[] args) throws Exception{
+	static int N, parent[];
+	static List<Integer>[] adjList;
+	
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st; 
 		
 		N = Integer.parseInt(br.readLine());
-		parents = new int[N+1];
-		v = new boolean[N+1];
+		adjList = new List[N+1];
+		parent = new int[N+1];
 		
-		adjList = new ArrayList[N+1]; 
+		Arrays.fill(parent, -2); 
 		
 		for(int i=1; i<=N; i++) {
 			adjList[i] = new ArrayList<>();
 		}
 		
-		for(int i=0; i<N-1; i++) {
+		StringTokenizer st;
+		for(int i=1; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int n1 = Integer.parseInt(st.nextToken());
-			int n2 = Integer.parseInt(st.nextToken());
 			
-			adjList[n1].add(n2);
-			adjList[n2].add(n1); 
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			adjList[a].add(b);
+			adjList[b].add(a);
 		}
 		
-		dfs(1); 
-		
-		StringBuilder sb = new StringBuilder(); 
-		
-		for(int i=2; i<N+1; i++) {
-		
-			sb.append(parents[i]).append("\n");
+		parent[1] = -1;
+		dfs(1, -1);
+		StringBuilder sb = new StringBuilder();
+		for(int i=2; i<=N; i++) {
+//			System.out.println(parent[i]);
+			sb.append(parent[i]).append("\n");
 		}
-		
-		
+			
 		System.out.println(sb);
+
 	}
 	
-	
-	
-	static void dfs(int parent) {
+	static void dfs(int cur, int prevNode) {
 		
-		ArrayList<Integer> cur = adjList[parent];
-		v[parent] = true;
+//		System.out.println(cur);
+		parent[cur] = prevNode;
 		
-		for(int i=0; i<cur.size(); i++) {
+		for(int nxt : adjList[cur]) {
+			if(parent[nxt] != -2) continue;
+			 
+			dfs(nxt, cur);
 			
-			int num = cur.get(i);
-			if(v[num]) continue; 
-			parents[num] = parent; 
-			dfs(num); 
 		}
 	}
 
