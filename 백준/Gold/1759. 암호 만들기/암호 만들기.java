929e937e75045c4df2287848da15712ace01efbe
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-03-19 10:01:21 +0900

    [Gold V] Title: 암호 만들기, Time: 72 ms, Memory: 11980 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,59 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-// 1759. 암호 만들기
 public class Main {
+
 	static int L, C;
-	static char[] alpha; 	// 전체 알파벳
-	static char[] sel; 		// 선택된 알파벳  
-
-	public static void main(String[] args) throws IOException {
+	static char[] alpha, sel; 
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		alpha = new char[C];
+		
 		st = new StringTokenizer(br.readLine());
-
-		L = Integer.parseInt(st.nextToken()); // 비밀번호에 선택된 문자 
-		C = Integer.parseInt(st.nextToken()); // 후보 문자
-		alpha = new char[C]; 
-		sel = new char[L]; 
-
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < C; i++) {
-			alpha[i] = st.nextToken().charAt(0); 
+		for(int i=0; i<C; i++) {
+			alpha[i] = st.nextToken().charAt(0);
 		}
+		
 		Arrays.sort(alpha);
-		combi(0, 0);
+		
+		sel = new char[L];
+		solve(0, 0);
+
 	}
-	private static void combi(int cnt, int start) {
+	
+	static void solve(int cnt, int start) {
 		if(cnt == L) {
-			StringBuilder sb = new StringBuilder(); 
-			int m = 0;
-			int j = 0; 
-			for(char c : sel) {
-				if(c == 'a' || c == 'e' ||c == 'i'|| c == 'o' ||c == 'u') {
-					m++; 
+//			System.out.println(Arrays.toString(sel));
+			StringBuilder sb = new StringBuilder();
+			
+			int vowel = 0;
+			int cons = 0;
+			
+			for(char a : sel) {
+				if(a == 'a' || a == 'e' || a == 'i' || a =='o' || a=='u') {
+					vowel++;
+				}else {
+					cons++;
 				}
-				else {
-					j++; 
-				}
+				
+				sb.append(a);
 			}
 			
-			if(m == 0 || j < 2) return; 
+			if(vowel < 1 || cons < 2) return; 
 			
-			for(char c : sel) {
-				sb.append(c); 
-			}
-			System.out.println(sb);	
+			System.out.println(sb);
+			
 			return; 
 		}
 		
-		for(int i=start;i<C;i++) {
-			sel[cnt] = alpha[i]; 
-			combi(cnt+1, i+1);
+		for(int i=start; i<C; i++) {
+			sel[cnt] = alpha[i];
+			solve(cnt+1, i+1);
 		}
 	}
 
