7c677f86d79af78361578998a16f42e9342ef850
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-04-16 15:00:44 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,30 +1,27 @@
--- History에서 대여일수 찾기
-WITH DURATION as (
-    SELECT 
-        b.DAILY_FEE, 
-        b.CAR_TYPE,
-        a.HISTORY_ID, 
-        DATEDIFF(end_date, start_date)+1 as period, 
-        CASE
-            WHEN DATEDIFF(end_date, start_date)+1 >= 90 THEN '90일 이상'
-            WHEN DATEDIFF(end_date, start_date)+1 >= 30 THEN '30일 이상'
-            WHEN DATEDIFF(end_date, start_date)+1 >= 7 THEN '7일 이상'
-            ELSE '해당없음'
-        END as duration_type
-    FROM
-        CAR_RENTAL_COMPANY_RENTAL_HISTORY a JOIN CAR_RENTAL_COMPANY_CAR b
-        ON a.CAR_ID = b.CAR_ID
-    WHERE
-        b.CAR_TYPE = '트럭'
+-- 코드를 입력하세요
+WITH TBL as (
+    
+SELECT a.car_id, daily_fee, HISTORY_ID,  
+DATEDIFF(end_date, start_date)+1 DIFF,
+    CASE 
+        WHEN DATEDIFF(end_date, start_date)+1 >= 90 THEN '90일 이상'
+        WHEN DATEDIFF(end_date, start_date)+1 >= 30 THEN '30일 이상'
+        WHEN DATEDIFF(end_date, start_date)+1 >= 7 THEN '7일 이상'
+        ELSE NULL
+    END as duration
+FROM CAR_RENTAL_COMPANY_CAR a INNER JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY b ON a.CAR_ID = b.CAR_ID
+WHERE a.CAR_TYPE = '트럭'
+
 )
 
-SELECT
-    a.HISTORY_ID,
-    ROUND(a.DAILY_FEE * a.period * (100 - IFNULL(b.discount_rate, 0)) / 100) as FEE
-FROM
-    DURATION a LEFT JOIN car_rental_company_discount_plan b
-    ON a.duration_type = b.duration_type
-    AND b.car_type = a.car_type
-ORDER BY 
-    2 DESC,
-    1 DESC
+# SELECT CAR_TYPE, DISCOUNT_RATE FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+# WHERE DURATION_TYPE = '90일 이상' AND CAR_TYPE = '트럭'
+
+
+SELECT HISTORY_ID, ROUND(diff * daily_fee * (1 - IFNULL((
+        SELECT discount_rate * 0.01
+        FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+        WHERE duration_type = tbl.duration AND car_type = '트럭'
+    ), 0)), 0) AS fee
+FROM TBL 
+ORDER BY 2 DESC, 1 DESC
