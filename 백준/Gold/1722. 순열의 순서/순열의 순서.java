51b5aabc6ffe16b6f494c01dbf6d7535ab62323e
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-04-29 19:07:13 +0900

    [Gold V] Title: 순열의 순서, Time: 64 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.java
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -1,62 +1,76 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
- 
- 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
- 
-        long[] fac = new long[21];
-        fac[0] = 1;
-        for(int i = 1; i<=20 ;i++) fac[i] = i*fac[i-1];
- 
-        int N = sc.nextInt();
-        int num = sc.nextInt();
-        long k = 0;
-        int[] arr = new int[N+1];
-        boolean[] visited = new boolean[N+1];
-        Arrays.fill(arr,0);
-        Arrays.fill(visited,false);
- 
-        Vector<Integer> ansVec = new Vector<Integer>();
-        long ans = 1;
- 
-        if (num==1){
-            k = sc.nextLong();      //Long으로 입력하지 않으면 런타임 에러뜸
-            for(int i = 0; i<N; i++){
-                for(int j = 1; j<=N; j++){
-                    if(visited[j]) continue;    //이미 사용된 숫자는 패스
-                    if(k - fac[N-1-i] > 0) {    //0이랑 같으면 1 3 4 2가 오답으로 출력됨 
-                        k -= fac[N-1-i];        
-                    }
-                    else {                         
-                        ansVec.add(j);
-                        visited[j] = true;
-                        break;
-                    }
-                }
-            }
-            for(int i = 0; i<ansVec.size(); i++){
-                System.out.print(ansVec.elementAt(i) + " ");
-            }
- 
-        }
-        else if (num==2){
-            for(int i = 0; i<N; i++){
-                arr[i] = sc.nextInt();  
-            }
-            for(int i = 0; i<N; i++){
-                for(int j = 1; j<arr[i]; j++){      //arr[i] 보다는 작은 수가 앞에 나올 수 있으므로 
-                    if(visited[j] == false){
-                        ans += fac[N-1-i];
-                    }
- 
-                }
-                visited[arr[i]] = true;
-            }
-            System.out.println(ans);
- 
- 
- 
-        }
-    }
+	
+	static int N, arr[];
+	static long fact[], K; 
+	static StringBuilder sb = new StringBuilder();
+	static List<Integer> list;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		fact = new long[N+1];
+		arr = new int[N+1];
+		
+		fact[1] = 1;
+		for(int i=2; i<N+1; i++) {
+			fact[i] = fact[i-1] * i; 
+		}
+		
+		list = new ArrayList<>();
+		for(int i=1; i<=N; i++) {
+			list.add(i);
+		}
+		
+		int type = Integer.parseInt(st.nextToken());
+		if(type == 1) {
+			K = Long.parseLong(st.nextToken())-1;
+			solve1(); 
+			
+		}else {
+			for(int i=1; i<=N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			solve2();
+		}
+		System.out.println(sb);
+	}
+	
+	static void solve1() {
+		
+		for(int i=1; i<N; i++) { // 현재 자릿수
+			long f = fact[N - i];
+			
+			int idx = (int) (K / f);
+			
+			K %= f; 
+			
+			sb.append(list.get(idx)).append(" ");
+			list.remove(idx);
+		}
+		
+		sb.append(list.get(0));
+	}
+	
+	static void solve2() {
+		long cnt = 1;
+		
+		for(int i=1; i<N; i++) {
+			int idx = list.indexOf(arr[i]);
+			
+			long f = fact[N-i];
+			
+			cnt += (idx * f);
+			
+			list.remove(idx);
+		}
+		
+		sb.append(cnt);
+	}
+
 }
