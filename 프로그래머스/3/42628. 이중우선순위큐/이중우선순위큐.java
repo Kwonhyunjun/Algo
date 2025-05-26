c6d1f0787879e929d691c5e7a40d72c1de2008ab
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-05-26 10:15:40 +0900

    [level 3] Title: 이중우선순위큐, Time: 53.40 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,41 +1,66 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
+    
+    static class Data{
+        int num; 
+        boolean isDeleted; 
+        
+        public Data(int num){
+            this.num = num;
+            this.isDeleted = false;
+        }
+    }
+    
     public int[] solution(String[] operations) {
-        int[] answer = new int[]{0, 0};
+        int[] answer = {};
+        
+        PriorityQueue<Data> min = new PriorityQueue<>((o1, o2) -> o1.num - o2.num);
+        PriorityQueue<Data> max = new PriorityQueue<>((o1, o2) -> -(o1.num - o2.num));
+        
         StringTokenizer st; 
         
-        TreeMap<Integer, Integer> map = new TreeMap<>(); 
-        
-        for(String s : operations){
-            st = new StringTokenizer(s, " "); 
-            int num = 0; 
-            switch(st.nextToken()){
-                case "I" :
-                    num = Integer.parseInt(st.nextToken());
-                    map.put(num, num); 
-                    break; 
-                case "D" :
-                    if(map.isEmpty()) continue;
-                    
-                    if(st.nextToken().equals("1")){
-                        int n1 = map.lastKey();
-                        map.remove(n1); 
-                        // System.out.printf("n1 : %d\n", n1);
-                    }else{
-                        int n2 = map.firstKey(); 
-                        map.remove(n2);
-                        // System.out.printf("n2 : %d\n", n2); 
-                    }
-                    break; 
+        for(String oper : operations){
+            st = new StringTokenizer(oper);
+            
+            char type = st.nextToken().charAt(0); 
+            
+            if(type == 'I'){
+                Data cur = new Data(Integer.parseInt(st.nextToken()));
+                
+                min.offer(cur);
+                max.offer(cur);
+                
+            }else{
+                while(!min.isEmpty() && min.peek().isDeleted){
+                    min.poll();
+                }
+                
+                while(!max.isEmpty() && max.peek().isDeleted){
+                    max.poll();
+                }
+                
+                PriorityQueue<Data> cur = (Integer.parseInt(st.nextToken()) == 1) ? max : min;
+                
+                if(cur.isEmpty()) continue;
+                
+                Data removed = cur.poll();
+                removed.isDeleted = true;
             }
         }
         
-        if(!map.isEmpty()){
-            answer[0] = map.lastKey(); 
-            answer[1] = map.firstKey(); 
+        while(!min.isEmpty() && min.peek().isDeleted){
+            min.poll();
+        }
+
+        while(!max.isEmpty() && max.peek().isDeleted){
+            max.poll();
         }
         
+        int minest = (min.isEmpty()) ? 0 : min.poll().num;
+        int maxest = (max.isEmpty()) ? 0 : max.poll().num;
+        
+        answer = new int[]{maxest, minest}; 
         
         return answer;
     }
