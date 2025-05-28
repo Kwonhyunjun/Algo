4b224054f1b1fea55f41125f24a7da1a611d8145
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-05-28 12:15:25 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 119.24 ms, Memory: 215 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,56 +1,26 @@
 import java.util.*; 
 
 class Solution {
-    /*
-        뒷 큰수 : 자신보다 뒤에 있는 숫자 중에서 자신보다 크면서 가장 가까이 있는 수를 뒷 큰수
-    */
     public int[] solution(int[] numbers) {
-        int len = numbers.length; 
-        int[] answer = new int[len];
+        int[] answer = {};
         
-        Stack<Integer> stack = new Stack<>();
-        Stack<Integer> temp = new Stack<>();
+        Stack<Integer> stack = new Stack();
         
-        int max = numbers[len-1]; 
-        stack.push(numbers[len-1]); 
-        answer[len-1] = -1; 
-        
-        for(int i=len-2; i>=0; i--){
+        answer = new int[numbers.length];
+        Arrays.fill(answer, -1);
+        stack.add(numbers[numbers.length-1]);
+        for(int i = numbers.length -2; i >= 0; i--){
             int cur = numbers[i];
-            // System.out.printf("cur : %d\n",cur); 
-            
-            if(cur >= max){
-                // System.out.printf("현재값이 최대값\n",cur); 
-                answer[i] = -1; 
-                max = cur; 
-                stack.push(cur);
-                continue;
+            // System.out.println("cur : " + cur);
+            // System.out.println(stack);
+            // System.out.println(stack.peek());
+            while(!stack.isEmpty() && cur >= stack.peek()){
+                stack.pop();
             }
+            // System.out.println(stack);
+            if(!stack.isEmpty()) answer[i] = stack.peek();
             
-            if(numbers[i] >= numbers[i+1] && numbers[i] < answer[i+1]){
-                // System.out.printf("%d\n", i); 
-                answer[i] = answer[i+1]; 
-                stack.push(cur); 
-                continue; 
-            }
-            
-            
-            while(stack.peek() <= cur){ 
-                // System.out.printf("대소비교 stack.peek() : %d vs cur : %d\n",stack.peek(), cur); 
-                
-                temp.push(stack.pop());
-            }
-            
-            // System.out.printf("최대값 찾은 후 : %d\n",stack.peek()); 
-            answer[i] = stack.peek(); 
-            
-            while(!temp.isEmpty()){
-                if(cur >= temp.pop()) continue;
-                stack.push(temp.pop()); 
-            }
-            
-            stack.push(cur); 
-            
+            stack.add(cur);
         }
         
         return answer;
