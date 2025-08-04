d9a1d4a48948844859da56dbed21a7f303fb104a
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2025-08-05 08:25:30 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 208 ms, Memory: 25748 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int N = Integer.parseInt(input[0]);
+        String type = input[1];
+        HashSet<String> friend = new HashSet<>();
+        for(int i=0; i<N; i++) {
+            friend.add(br.readLine());
+        }
+
+        switch (type) {
+            case "Y":
+                System.out.print(friend.size());
+                break;
+            case "F":
+                System.out.print(friend.size()/2);
+                break;
+            case "O":
+                System.out.print(friend.size()/3);
+                break;
+            default:
+                break;
+        }
+        
+    }
+}
